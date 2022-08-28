e887f9c9bd896468d0ec169e2d085810d3473ffb
Marcio Cardoso <marcioliveiracardoso@gmail.com>
2022-08-28 17:19:32 -0300

    agencia de turismo

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -2,4 +2,16 @@ let nome
 let email
 let profissão
 let idade
-let lugar que sonha conhecer
+let lugarQueSonhaConhecer
+
+nome = "Andréia"
+email = "cardoso.deia@gmail.com"
+profissão = "Gestora de Rh"
+idade = "44"
+lugarQueSonhaConhecer = "Itália"
+
+console.log(nome)
+console.log(email)
+console.log(profissão)
+console.log(idade)
+console.log(lugarQueSonhaConhecer)
